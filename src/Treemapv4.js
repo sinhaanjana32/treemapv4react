c1e0a1e58da673fd3b03531325086a98b1d6eb3e
Anjana <sinhaanjana32@gmail.com>
2021-02-18 12:34:59 +0200

    converting code in react

---
+++ src/Treemapv4.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+const Treemapv4 = () => {
+  useEffect(() => {}, []);
+
+  return (
+    <div id="app">
+      <svg></svg>
+    </div>
+  );
+};
+
+export default Treemapv4;
